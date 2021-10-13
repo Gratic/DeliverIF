b7b5b6219f350bfa4f29f80e1d904e30e183f90f
onyr <rascoussier.florian@gmail.com>
2021-10-13 11:59:14 +0200

    Small modifs.

--- src/main/java/deliverif/controller/Controller.java
+++ src/main/java/deliverif/controller/Controller.java
@@ -1,5 +1,6 @@
 package deliverif.controller;
 
+import deliverif.model.CityMap;
 import deliverif.view.View;
 import deliverif.controller.state.*;
 
@@ -7,17 +8,19 @@ public class Controller {
     
     private View view;
     private State currentState;
+    private CityMap cityMap;
 
     protected State initState;
     
     public Controller() {
         view = new View();
+        currentState = new InitState();
+        cityMap = new CityMap();
+
         init();
     }
     
     public void init() {
-        currentState = new InitState();
-
         // states, solution 3
         initState = new InitState();
         
@@ -27,20 +30,18 @@ public class Controller {
     // state functions
     public void load() {
         currentState.load();
-    };
+    }
     public void undo() {
         currentState.undo();
-    };
+    }
     public void redo() {
         currentState.redo();
-    };
+    }
     public void rightClick(Controller controller, View view) {
         currentState.rightClick(this, view);
     }
     public void leftClick(Controller controller, View view) {
         currentState.leftClick(this, view);
     }
-    
-    
-    
+
 }
