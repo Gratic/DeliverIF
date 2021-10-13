1f1f175ea9c32d69a27aaa58b9971958de9fa25e
onyr <rascoussier.florian@gmail.com>
2021-10-13 11:34:39 +0200

    Added base classes for ihm.

--- src/main/java/deliverif/Main.java
+++ src/main/java/deliverif/Main.java
@@ -1,8 +1,11 @@
 package deliverif;
 
+import deliverif.controller.Controller;
+
 public class Main
 {
     public static void main(String[] args) {
 
+        Controller controller = new Controller();
     }
 }
