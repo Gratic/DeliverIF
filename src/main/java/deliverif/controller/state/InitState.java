cc0c49d4ada40b0cbd686df6ed6caa32f48855cd
onyr <rascoussier.florian@gmail.com>
2021-10-14 11:33:50 +0200

    First test of State implementation.

--- src/main/java/deliverif/controller/state/InitState.java
+++ src/main/java/deliverif/controller/state/InitState.java
@@ -1,15 +1,31 @@
 package deliverif.controller.state;
 
+import deliverif.controller.Controller;
+import deliverif.model.CityMap;
+import deliverif.view.View;
+
+import java.io.File;
+
 public class InitState implements State {
     
     public InitState() {
 
     }
 
-    public void load() {
+    public void loadMapButtonClick(Controller controller, View view) {
+        // get path to file
+        String path = view.filePathField.getText();
 
+        // load map
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(path).getFile());
+
+        // tells the model to load its data
+        CityMap cityMap = controller.getCityMap();
+        cityMap.loadMapFromFile(file);
+
+        // change to next state
+        controller.setCurrentState(controller.loadState);
     }
-
-    
     
 }
