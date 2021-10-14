5eea1cff65ed3d3a6df03b361967d7a1fb32aa5d
onyr <rascoussier.florian@gmail.com>
2021-10-14 11:33:50 +0200

    First test of State implementation.

--- src/main/java/deliverif/view/View.java
+++ src/main/java/deliverif/view/View.java
@@ -1,14 +1,24 @@
 package deliverif.view;
 
 import javax.swing.*;
+import deliverif.controller.Controller;
 
-public class View {
+import java.awt.event.*;
+
+public class View implements ActionListener {
+
+    protected Controller controller;
+
+    // testing purpose
+    public JTextField filePathField;
     
-    public View() {
+    public View(Controller controller) {
         
         // vue test
-        JFrame f=new JFrame();//creating instance of JFrame  
-        JButton b=new JButton("click");//creating instance of JButton  
+        JFrame f=new JFrame();//creating instance of JFrame
+        JTextField filePathField = new JTextField(50);
+        filePathField.setLocation(50, 50);
+        JButton b=new JButton("Load");//creating instance of JButton
         b.setBounds(130,100,100, 40);
 
         f.add(b);//adding button in JFrame  
@@ -21,9 +31,15 @@ public class View {
         JFrame cityMapFrame = new JFrame();
 
     }
-    
+
     public void init() {
         // TODO: complete init()
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // load action
+        controller.buttonClick(this);
     }
     
 }
