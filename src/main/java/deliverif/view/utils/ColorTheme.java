148359d53b37d7e28c0c96d3d661bfacc74e4721
onyr <rascoussier.florian@gmail.com>
2021-10-16 17:33:59 +0200

    Finally made some progress with layouts.

---
+++ src/main/java/deliverif/view/utils/ColorTheme.java
@@ -0,0 +1,18 @@
+package deliverif.view.utils;
+
+
+import java.awt.*;
+
+public class ColorTheme {
+    // palette1: https://paletton.com/#uid=3170T0k2ljc6hnI3vhb50fr6wdx
+
+    // palette 1: base colors for panels
+    public static Color MEDIUM_BASE_GREY = new Color(0x616966);
+    public static Color LIGHT_BASE_GREY = new Color(0x99968E);
+    public static Color MEDIUM_BASE_PURPLE = new Color(0x635E65);
+
+    // general theme settings
+    public static Color GENERAL_BASE_BG = LIGHT_BASE_GREY;
+    public static Color PANEL_1_BASE_BG = MEDIUM_BASE_GREY;
+    public static Color PANEL_2_BASE_BG = MEDIUM_BASE_PURPLE;
+}
