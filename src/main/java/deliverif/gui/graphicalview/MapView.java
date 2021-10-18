793079629df6d52dcc77acc1d497f9795d45532b
Romain Gallé <romain.galle@insa-lyon.fr>
2021-10-18 22:45:32 +0200

    feat: Added zoom and move options on mapview

--- src/main/java/deliverif/gui/graphicalview/MapView.java
+++ src/main/java/deliverif/gui/graphicalview/MapView.java
@@ -8,9 +8,10 @@ import deliverif.observer.Observer;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.util.Collection;
 
-public class MapView extends JPanel implements Observer {
+public class MapView extends JPanel implements Observer, MouseWheelListener, MouseMotionListener {
     private CityMap map;
 
     private double
@@ -21,9 +22,18 @@ public class MapView extends JPanel implements Observer {
 
     private final int DEFAULT_WIDTH = 600, DEFAULT_HEIGHT = 400;
 
+    private final double MAX_ZOOM_LEVEL = 6.0;
+    private final double MIN_ZOOM_LEVEL = 1.0;
+    private final double ZOOM_SENSITIVITY = 0.2;
+    private double zoomLevel = MIN_ZOOM_LEVEL;
+
+    private int xTranslation = 0, yTranslation = 0;
+
     public MapView(CityMap map) {
         this.setPreferredSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
         this.setBackground(new Color(213, 213, 213));
+        this.addMouseMotionListener(this);
+        this.addMouseWheelListener(this);
 
         this.map = map;
         this.recomputeMinMaxLatLong();
@@ -34,7 +44,7 @@ public class MapView extends JPanel implements Observer {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        if(this.isMapLoaded()) {
+        if (this.isMapLoaded()) {
             g.setColor(new Color(10, 10, 10));
 
             for (Address address : this.map.getAddresses().values()) {
@@ -52,8 +62,7 @@ public class MapView extends JPanel implements Observer {
                     }
                 }
             }
-        }
-        else {
+        } else {
             String message = "La carte n'est pas chargée ou vide.";
             int messageWidth = g.getFontMetrics().stringWidth(message);
             g.drawString(message, getHeight() / 2, (getWidth() - messageWidth) / 2);
@@ -64,8 +73,8 @@ public class MapView extends JPanel implements Observer {
         double normalizedLatitude = (latitude - latitudeMin) / (latitudeMax - latitudeMin);
         double normalizedLongitude = (longitude - longitudeMin) / (longitudeMax - longitudeMin);
 
-        int y = 800 - (int)(normalizedLatitude * 800);
-        int x = (int)(normalizedLongitude * 800);
+        int y = 800 - (int) (normalizedLatitude * 800 * zoomLevel) + yTranslation;
+        int x = (int) (normalizedLongitude * 800 * zoomLevel) + xTranslation;
         return new Point(x, y);
     }
 
@@ -75,7 +84,7 @@ public class MapView extends JPanel implements Observer {
     }
 
     private void recomputeMinMaxLatLong() {
-        if(!this.isMapLoaded()) return;
+        if (!this.isMapLoaded()) return;
 
         for (Address address : map.getAddresses().values()) {
             if (address.getLatitude() > latitudeMax) {
@@ -100,5 +109,85 @@ public class MapView extends JPanel implements Observer {
     @Override
     public void update(Observable observed, Object arg) {
         this.recomputeMinMaxLatLong();
+        this.repaint();
+    }
+
+    private Point lastPointDragged = null;
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        if (this.lastPointDragged != null) {
+            int xMove = e.getX() - this.lastPointDragged.x;
+            int yMove = e.getY() - this.lastPointDragged.y;
+
+            this.translateView(xMove, yMove);
+        }
+
+        this.lastPointDragged = e.getPoint();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        this.lastPointDragged = null;
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+        int wheelRotationAbs = Math.abs(e.getWheelRotation());
+        if (e.getWheelRotation() > 0) { // positive is unzoom
+            this.zoomView(-ZOOM_SENSITIVITY * wheelRotationAbs);
+        } else if (e.getWheelRotation() < 0) { // negative is zoom
+            this.zoomView(ZOOM_SENSITIVITY * wheelRotationAbs);
+        }
+    }
+
+    public void zoomView(double zoomDelta) {
+        double prevZoomLevel = this.zoomLevel;
+
+        this.zoomLevel += zoomDelta;
+
+        if (this.zoomLevel < MIN_ZOOM_LEVEL) {
+            this.zoomLevel = MIN_ZOOM_LEVEL;
+        } else if (this.zoomLevel > MAX_ZOOM_LEVEL) {
+            this.zoomLevel = MAX_ZOOM_LEVEL;
+        }
+
+        this.repaint();
+/*
+        double absoluteZoomDelta = this.zoomLevel - prevZoomLevel;
+        // double relativeZoomDelta = absoluteZoomDelta / this.zoomLevel;
+        double relativeZoomDelta = this.zoomLevel / prevZoomLevel;
+        System.out.println(relativeZoomDelta);
+        //if(absoluteZoomDelta > 0.0) {
+            Point farthestPoint = this.latlongToXY(this.latitudeMax, this.longitudeMax);
+            System.out.println(absoluteZoomDelta );
+
+            /*int widthDelta = (int) (farthestPoint.x * (absoluteZoomDelta / this.zoomLevel) / 2.0);
+            int heightDelta = (int) (farthestPoint.y * (absoluteZoomDelta / this.zoomLevel) / 2.0);
+
+            int widthDelta = (int)(farthestPoint.x * (relativeZoomDelta - 1.0));
+            int heightDelta = (int)(farthestPoint.y * (relativeZoomDelta - 1.0));
+
+            int widthDeltaAbs = (int)(farthestPoint.x * absoluteZoomDelta);
+            int heightDeltaAbs = (int)(farthestPoint.y * absoluteZoomDelta);
+
+            System.out.println(widthDelta + " " + heightDelta);
+            System.out.println(widthDeltaAbs + " " + heightDeltaAbs);
+            this.translateView(-widthDelta, -heightDelta);
+//        }*/
+    }
+
+    public void translateView(int deltaX, int deltaY) {
+        this.xTranslation += deltaX;
+        this.yTranslation += deltaY;
+/*
+        Point farthestPoint = this.latlongToXY(this.latitudeMax, this.longitudeMax);
+
+        // TODO this equation does not handles maps smaller than main window
+        if (this.xTranslation + this.getWidth() > farthestPoint.x) {
+            System.out.println("X out of bounds !");
+        }
+*/
+        this.repaint();
     }
 }
