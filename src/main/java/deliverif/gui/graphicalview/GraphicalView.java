d32c12a6b8e074a32e71c881211c84457d76d7d2
Romain Gallé <romain.galle@insa-lyon.fr>
2021-10-18 10:42:06 +0200

    Implemented first version of map display

---
+++ src/main/java/deliverif/gui/graphicalview/GraphicalView.java
@@ -0,0 +1,96 @@
+package deliverif.view.graphicalview;
+
+import deliverif.model.Address;
+import deliverif.model.CityMap;
+import deliverif.model.RoadSegment;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Collection;
+
+public class GraphicalView extends JPanel {
+    private CityMap map;
+
+    private double
+            latitudeMin = Double.MAX_VALUE,
+            latitudeMax = Double.MIN_VALUE,
+            longitudeMin = Double.MAX_VALUE,
+            longitudeMax = Double.MIN_VALUE;
+
+    private final int width = 600, height = 400;
+
+    public GraphicalView(CityMap map) {
+        this.setPreferredSize(new Dimension(width, height));
+        this.setBackground(new Color(213, 213, 213));
+
+        this.map = map;
+        this.recomputeMinMaxLatLong();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if(this.isMapLoaded()) {
+            g.setColor(new Color(10, 10, 10));
+
+            for (Address address : this.map.getAddresses().values()) {
+                // Point point = this.latlongToXY(address.getLatitude(), address.getLongitude());
+                // g.fillOval(point.x - (int) (10 / 2), point.y - (int) (10 / 2), 10, 10);
+
+                Collection<RoadSegment> segments = this.map.getSegmentsOriginatingFrom(address.getId());
+                if (segments != null) {
+                    for (RoadSegment segment : segments) {
+                        Point startCoord = this.latlongToXY(segment.getOrigin().getLatitude(),
+                                segment.getOrigin().getLongitude());
+                        Point endCoord = this.latlongToXY(segment.getDestination().getLatitude(), segment.getDestination().getLongitude());
+
+                        g.drawLine(startCoord.x, startCoord.y, endCoord.x, endCoord.y);
+                    }
+                }
+            }
+        }
+        else {
+            String message = "La carte n'est pas chargée ou vide.";
+            int messageWidth = g.getFontMetrics().stringWidth(message);
+            g.drawString(message, getHeight() / 2, (getWidth() - messageWidth) / 2);
+        }
+    }
+
+    private Point latlongToXY(double latitude, double longitude) {
+        double normalizedLatitude = (latitude - latitudeMin) / (latitudeMax - latitudeMin);
+        double normalizedLongitude = (longitude - longitudeMin) / (longitudeMax - longitudeMin);
+
+        int y = 800 - (int)(normalizedLatitude * 800);
+        int x = (int)(normalizedLongitude * 800);
+        return new Point(x, y);
+    }
+
+    public void setMap(CityMap map) {
+        this.map = map;
+        this.recomputeMinMaxLatLong();
+    }
+
+    private void recomputeMinMaxLatLong() {
+        if(!this.isMapLoaded()) return;
+
+        for (Address address : map.getAddresses().values()) {
+            if (address.getLatitude() > latitudeMax) {
+                latitudeMax = address.getLatitude();
+            }
+            if (address.getLatitude() < latitudeMin) {
+                latitudeMin = address.getLatitude();
+            }
+            if (address.getLongitude() > longitudeMax) {
+                longitudeMax = address.getLongitude();
+            }
+            if (address.getLongitude() < longitudeMin) {
+                longitudeMin = address.getLongitude();
+            }
+        }
+    }
+
+    private boolean isMapLoaded() {
+        return !(map == null || map.getAddresses().size() == 0);
+    }
+}
