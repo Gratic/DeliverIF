52bacb1c7661e7146f8142181b4a46c59863e2b2
Thibaud <tpires@peaks.fr>
2021-11-04 09:48:50 +0100

    feat: implemented wrappers to acces shortestpath algorithms

---
+++ src/main/java/pdtsp/BasicGraphWrapper.java
@@ -0,0 +1,87 @@
+package pdtsp;
+
+import deliverif.model.*;
+
+import java.util.*;
+
+/**
+ * This class is the bridge between the model representation of a map and the algorithmic one
+ */
+public class BasicGraphWrapper {
+
+
+    private final Map<Long, Integer> longToInt;
+    private final Map<Integer, Long> intToLong;
+    private final Graph graph;
+    private final CityMap cityMap;
+    private final RoadSegment[][] roadSegmentsMatrix;
+
+    public BasicGraphWrapper(CityMap map) {
+
+
+        this.longToInt = new HashMap<>();
+        this.intToLong = new HashMap<>();
+        Map<Integer, Collection<Pair<Integer, Double>>> cityMapInteger = new HashMap<>();
+
+
+        this.cityMap = map;
+
+        // TASK 1: Re-index long indexes into integer indexes because BasicGraph only takes integer.
+        int currentIntegerIndex = 0;
+
+        for (Long key : cityMap.getAddresses().keySet()) {
+            longToInt.put(key, currentIntegerIndex);
+            intToLong.put(currentIntegerIndex, key);
+            currentIntegerIndex++;
+        }
+
+        this.roadSegmentsMatrix = new RoadSegment[longToInt.size()][longToInt.size()];
+
+        // TASK 2: Building the integer graph
+        for (Long longNodeIndex : cityMap.getAddresses().keySet()) {
+            int integerOriginNodeIndex = longToInt.get(longNodeIndex);
+
+            // Pair<node ID, distance>
+            Collection<Pair<Integer, Double>> arcs = new ArrayList<>();
+
+            Collection<RoadSegment> outgoingRoadSegments = cityMap.getSegments().get(longNodeIndex);
+
+            if (outgoingRoadSegments != null) {
+                for (RoadSegment rs : outgoingRoadSegments) {
+                    int integerDestinationNodeIndex = longToInt.get(rs.getDestination().getId());
+                    double roadSegmentLength = rs.getLength();
+
+                    roadSegmentsMatrix[integerOriginNodeIndex][integerDestinationNodeIndex] = rs;
+                    arcs.add(new Pair<>(integerDestinationNodeIndex, roadSegmentLength));
+                }
+            }
+
+
+            cityMapInteger.put(integerOriginNodeIndex, arcs);
+        }
+
+
+        this.graph = new BasicGraph(cityMapInteger.size(), cityMapInteger);
+
+    }
+
+    public CityMap getMap() {
+        return cityMap;
+    }
+
+    public Graph getGraph() {
+        return graph;
+    }
+
+    public Integer idConvert(long id) {
+        return longToInt.get(id);
+    }
+
+    public long idConvert(Integer id) {
+        return intToLong.get(id);
+    }
+
+    public RoadSegment getSegment(int from, int to) {
+        return roadSegmentsMatrix[from][to];
+    }
+}
--- src/main/java/pdtsp/PDTSPWrapper.java
+++ src/main/java/pdtsp/PDTSPWrapper.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 /**
  * This class is the bridge between the real world and the algorithmic world.
- *
+ * <p>
  * It is also the only class to call if there is a need for a TSP Computation.
  */
 public class PDTSPWrapper {
@@ -26,106 +26,63 @@ public class PDTSPWrapper {
 
     boolean isPreparationDone;
 
-    private final Map<Long, Integer> longToInt;
-    private final Map<Integer, Long> intToLong;
-    private final Graph map;
-    private final CityMap cityMap;
-    private final RoadSegment[][] roadSegmentsMatrix;
+    private final BasicGraphWrapper graphWrapper;
+
     private final DeliveryTour deliveryTour;
 
-    public PDTSPWrapper(CityMap cityMap, DeliveryTour deliveryTour, Integer timeLimit)
-    {
+    public PDTSPWrapper(CityMap cityMap, DeliveryTour deliveryTour, Integer timeLimit) {
         this.timeLimit = timeLimit;
 
-        this.longToInt = new HashMap<>();
-        this.intToLong = new HashMap<>();
-        Map<Integer, Collection<Pair<Integer, Double>>> cityMapInteger = new HashMap<>();
+
         this.requestsInteger = new ArrayList<>();
 
-        this.cityMap = cityMap;
+
         this.deliveryTour = deliveryTour;
         this.requests = deliveryTour.getRequests();
 
         Address startPoint = deliveryTour.getDepartureAddress();
 
         // TASK 1: Re-index long indexes into integer indexes because BasicGraph only takes integer.
-        int currentIntegerIndex = 0;
+        this.graphWrapper = new BasicGraphWrapper(cityMap);
 
-        for(Long key : cityMap.getAddresses().keySet())
-        {
-            longToInt.put(key, currentIntegerIndex);
-            intToLong.put(currentIntegerIndex, key);
-            currentIntegerIndex++;
-        }
-
-        this.startPointInteger = longToInt.get(startPoint.getId());
-
-        this.roadSegmentsMatrix = new RoadSegment[longToInt.size()][longToInt.size()];
+        this.startPointInteger = graphWrapper.idConvert(startPoint.getId());
 
         // TASK 2: Building the integer graph
-        for(Long longNodeIndex : cityMap.getAddresses().keySet())
-        {
-            int integerOriginNodeIndex = longToInt.get(longNodeIndex);
 
-            // Pair<node ID, distance>
-            Collection<Pair<Integer, Double>> arcs = new ArrayList<>();
-
-            Collection<RoadSegment> outgoingRoadSegments = cityMap.getSegments().get(longNodeIndex);
-
-            if(outgoingRoadSegments != null)
-            {
-                for(RoadSegment rs : outgoingRoadSegments)
-                {
-                    int integerDestinationNodeIndex = longToInt.get(rs.getDestination().getId());
-                    double roadSegmentLength = rs.getLength();
-
-                    roadSegmentsMatrix[integerOriginNodeIndex][integerDestinationNodeIndex] = rs;
-                    arcs.add(new Pair<>(integerDestinationNodeIndex, roadSegmentLength));
-                }
-            }
-
-
-            cityMapInteger.put(integerOriginNodeIndex, arcs);
-        }
 
         // TASK 3: Compute the integer requests
 
         // Request order is (should be) conserved.
-        for(Request requestObj : requests)
-        {
+        for (Request requestObj : requests) {
             long longPickupNode = requestObj.getPickupAddress().getId();
             long longDeliveryNode = requestObj.getDeliveryAddress().getId();
 
-            int integerPickupNode = longToInt.get(longPickupNode);
-            int integerDeliveryNode = longToInt.get(longDeliveryNode);
+            int integerPickupNode = graphWrapper.idConvert(longPickupNode);
+            int integerDeliveryNode = graphWrapper.idConvert(longDeliveryNode);
 
             requestsInteger.add(new Pair<>(integerPickupNode, integerDeliveryNode));
         }
 
         // Finally, we can create the graph.
-        this.map = new BasicGraph(cityMapInteger.size(), cityMapInteger);
 
         isPreparationDone = false;
     }
 
-    public void prepare()
-    {
-        if(!isPreparationDone)
-        {
+    public void prepare() {
+        if (!isPreparationDone) {
             // TASK 1: Reevaluation
             ShortestPath dijkstra = new Dijkstra();
-            reevaluator = new ReevaluationTransformer(map, requestsInteger, startPointInteger, dijkstra);
+            reevaluator = new ReevaluationTransformer(graphWrapper.getGraph(), requestsInteger, startPointInteger, dijkstra);
             reevaluator.transform();
 
             Graph reevaluated = reevaluator.getTransformedGraph();
             List<Pair<Integer, Integer>> reevaluatedRequests = reevaluator.getTransformedRequests();
 
-            if(DEBUG)
-            {
+            if (DEBUG) {
                 System.out.println("reevaluated");
-                System.out.println("concerned indexes=" +reevaluator.getConcernedIndexes());
-                System.out.println("reevaluatedPortalIn=" +reevaluator.getBeforeToAfterIndexes());
-                System.out.println("reevaluatedPortalOut=" +reevaluator.getAfterToBeforeIndexes());
+                System.out.println("concerned indexes=" + reevaluator.getConcernedIndexes());
+                System.out.println("reevaluatedPortalIn=" + reevaluator.getBeforeToAfterIndexes());
+                System.out.println("reevaluatedPortalOut=" + reevaluator.getAfterToBeforeIndexes());
                 BasicGraph.printGraph(reevaluated);
             }
 
@@ -143,20 +100,16 @@ public class PDTSPWrapper {
 
             isPreparationDone = true;
 
-            if(pdtsp == null)
-            {
+            if (pdtsp == null) {
                 pdtsp = new BnBPDTSP();
             }
         }
 
     }
 
-    public void compute()
-    {
-        if(isPreparationDone)
-        {
-            if(DEBUG)
-            {
+    public void compute() {
+        if (isPreparationDone) {
+            if (DEBUG) {
                 System.out.println("distorted");
                 BasicGraph.printGraph(distorter.getTransformedGraph());
                 System.out.println("p");
@@ -167,8 +120,7 @@ public class PDTSPWrapper {
         }
     }
 
-    public List<Long> getPath()
-    {
+    public List<Long> getPath() {
         Graph distorted = distorter.getTransformedGraph();
         Map<Integer, Integer> distortedPortalOut = distorter.getAfterToBeforeIndexes();
         Map<Integer, Integer> reevaluationPortalOut = reevaluator.getAfterToBeforeIndexes();
@@ -179,8 +131,7 @@ public class PDTSPWrapper {
         int previous = reevaluationPortalOut.get(distortedPortalOut.get(0));
         realPathInteger.add(previous);
 
-        for(int i = 1; i < distorted.getNbVertices(); i++)
-        {
+        for (int i = 1; i < distorted.getNbVertices(); i++) {
             int node = pdtsp.getSolution(i);
             int currentNode = reevaluationPortalOut.get(distortedPortalOut.get(node));
 
@@ -191,73 +142,55 @@ public class PDTSPWrapper {
 
         realPathInteger.addAll(getRealPath(previous, startPointInteger, shortestPaths));
 
-        for(int node : realPathInteger)
-        {
-            realPath.add(intToLong.get(node));
+        for (Integer node : realPathInteger) {
+            realPath.add(graphWrapper.idConvert(node));
         }
 
-        if(DEBUG)
-        {
+        if (DEBUG) {
             System.out.println("Path found :");
             System.out.print("[");
-            for(int i = 0; i < distorted.getNbVertices(); i++)
-            {
+            for (int i = 0; i < distorted.getNbVertices(); i++) {
                 int node = pdtsp.getSolution(i);
-                if(i == distorted.getNbVertices()-1)
-                {
+                if (i == distorted.getNbVertices() - 1) {
                     System.out.print(node);
-                }
-                else
-                {
-                    System.out.print(node+",");
+                } else {
+                    System.out.print(node + ",");
                 }
             }
             System.out.println("]");
-            System.out.println("SolutionCost="+pdtsp.getSolutionCost());
+            System.out.println("SolutionCost=" + pdtsp.getSolutionCost());
             System.out.println("Conversion in reevaluated World :");
             System.out.print("[");
-            for(int i = 0; i < distorted.getNbVertices(); i++)
-            {
+            for (int i = 0; i < distorted.getNbVertices(); i++) {
                 int node = pdtsp.getSolution(i);
-                if(i == distorted.getNbVertices()-1)
-                {
+                if (i == distorted.getNbVertices() - 1) {
                     System.out.print(distortedPortalOut.get(node));
-                }
-                else
-                {
-                    System.out.print(distortedPortalOut.get(node)+",");
+                } else {
+                    System.out.print(distortedPortalOut.get(node) + ",");
                 }
             }
             System.out.println("]");
 
             System.out.println("Conversion in real World :");
             System.out.print("[");
-            for(int i = 0; i < distorted.getNbVertices(); i++)
-            {
+            for (int i = 0; i < distorted.getNbVertices(); i++) {
                 int node = pdtsp.getSolution(i);
-                if(i == distorted.getNbVertices()-1)
-                {
+                if (i == distorted.getNbVertices() - 1) {
                     System.out.print(reevaluationPortalOut.get(distortedPortalOut.get(node)));
-                }
-                else
-                {
-                    System.out.print(reevaluationPortalOut.get(distortedPortalOut.get(node))+",");
+                } else {
+                    System.out.print(reevaluationPortalOut.get(distortedPortalOut.get(node)) + ",");
                 }
             }
             System.out.println("]");
 
             System.out.println("Realpath:");
             System.out.print("[");
-            for(int i = 0; i < realPathInteger.size(); i++)
-            {
+            for (int i = 0; i < realPathInteger.size(); i++) {
                 int node = realPathInteger.get(i);
-                if(i == realPathInteger.size()-1)
-                {
+                if (i == realPathInteger.size() - 1) {
                     System.out.print(node);
-                }
-                else
-                {
-                    System.out.print(node+",");
+                } else {
+                    System.out.print(node + ",");
                 }
             }
             System.out.println("]");
@@ -266,8 +199,7 @@ public class PDTSPWrapper {
         return realPath;
     }
 
-    public void updateDeliveryTour()
-    {
+    public void updateDeliveryTour() {
         Graph distorted = distorter.getTransformedGraph();
         Map<Integer, Integer> distortedPortalOut = distorter.getAfterToBeforeIndexes();
         Map<Integer, String> distortedNodeTypes = distorter.getNodeTypes();
@@ -290,15 +222,13 @@ public class PDTSPWrapper {
         realPathInteger.add(previous);
 
         // All nodes except Starting node.
-        for(int i = 1; i < distorted.getNbVertices(); i++)
-        {
+        for (int i = 1; i < distorted.getNbVertices(); i++) {
             int node = pdtsp.getSolution(i);
             String nodeType = distortedNodeTypes.get(node);
             int currentNode = reevaluationPortalOut.get(distortedPortalOut.get(node));
 
             List<Integer> tempRealPath = getRealPath(previous, currentNode, shortestPaths);
-            for(int j = 0; j < tempRealPath.size() - 1; j++)
-            {
+            for (int j = 0; j < tempRealPath.size() - 1; j++) {
                 listTypes.add("traversal");
                 listNodeRequest.add(null);
             }
@@ -312,8 +242,7 @@ public class PDTSPWrapper {
 
         // From last node to Starting node.
         List<Integer> tempRealPath = getRealPath(previous, startPointInteger, shortestPaths);
-        for(int j = 0; j < tempRealPath.size() - 1; j++)
-        {
+        for (int j = 0; j < tempRealPath.size() - 1; j++) {
             listTypes.add("traversal");
             listNodeRequest.add(null);
         }
@@ -323,9 +252,8 @@ public class PDTSPWrapper {
         realPathInteger.addAll(getRealPath(previous, startPointInteger, shortestPaths));
 
         // Conversion in real world node ID.
-        for(int node : realPathInteger)
-        {
-            realPath.add(intToLong.get(node));
+        for (Integer node : realPathInteger) {
+            realPath.add(graphWrapper.idConvert(node));
         }
 
         // TASK 2: Updating DeliveryTour.
@@ -333,16 +261,15 @@ public class PDTSPWrapper {
         deliveryTour.getPathAddresses().clear();
         deliveryTour.getAddressRequestMetadata().clear();
 
-        for(int i = 0; i < realPath.size()-1; i++)
-        {
+        for (int i = 0; i < realPath.size() - 1; i++) {
             long longCurrentNode = realPath.get(i);
-            long longNextNode = realPath.get(i+1);
+            long longNextNode = realPath.get(i + 1);
 
-            int intCurrentNode = longToInt.get(longCurrentNode);
-            int intNextNode = longToInt.get(longNextNode);
+            int intCurrentNode = graphWrapper.idConvert(longCurrentNode);
+            int intNextNode = graphWrapper.idConvert(longNextNode);
 
-            Address currentAddress = cityMap.getAddressById(longCurrentNode);
-            RoadSegment rsCurrentToNext = roadSegmentsMatrix[intCurrentNode][intNextNode];
+            Address currentAddress = graphWrapper.getMap().getAddressById(longCurrentNode);
+            RoadSegment rsCurrentToNext = graphWrapper.getSegment(intCurrentNode, intNextNode);
 
             String nodeType = listTypes.get(i);
             switch (nodeType) {
@@ -369,25 +296,23 @@ public class PDTSPWrapper {
         }
 
         // We add the last address.
-        deliveryTour.addAddress(cityMap.getAddressById(realPath.get(realPath.size()-1)), EnumAddressType.DEPARTURE_ADDRESS, null);
+        deliveryTour.addAddress(graphWrapper.getMap().getAddressById(realPath.get(realPath.size() - 1)), EnumAddressType.DEPARTURE_ADDRESS, null);
     }
 
     /**
      * Get the real (shortest) path between to node.
      * Starting node is not included.
-     *
+     * <p>
      * NOTE: this must be used with REAL WORLD node from the REAL WORLD graph (a.k.a map).
      */
-    private static List<Integer> getRealPath(Integer i, Integer j, Map<Integer, List<Pair<Double, Integer>>> shortestPaths)
-    {
+    private static List<Integer> getRealPath(Integer i, Integer j, Map<Integer, List<Pair<Double, Integer>>> shortestPaths) {
         List<Pair<Double, Integer>> result = shortestPaths.get(i);
         List<Integer> path = new ArrayList<>();
 
         path.add(j);
 
         Integer currentNode = result.get(j).getY();
-        while(!currentNode.equals(i) && !currentNode.equals(-1))
-        {
+        while (!currentNode.equals(i) && !currentNode.equals(-1)) {
             path.add(currentNode);
             currentNode = result.get(currentNode).getY();
         }
@@ -397,12 +322,15 @@ public class PDTSPWrapper {
         return path;
     }
 
-    public boolean isSolutionFound()
-    {
+    public boolean isSolutionFound() {
         return pdtsp.isSolutionFound();
     }
 
-    public boolean isSolutionOptimal() { return pdtsp.isSolutionOptimal(); }
+    public boolean isSolutionOptimal() {
+        return pdtsp.isSolutionOptimal();
+    }
 
-    public double getSolutionCost() { return pdtsp.getSolutionCost(); }
+    public double getSolutionCost() {
+        return pdtsp.getSolutionCost();
+    }
 }
