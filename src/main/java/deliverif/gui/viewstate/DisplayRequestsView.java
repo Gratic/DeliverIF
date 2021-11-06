f78b525b9a07d8c66b6c78852749e901ad869fe6
Thibaud <tpires@peaks.fr>
2021-11-06 18:29:41 +0100

    feat: Panels are now ordered acording to path when tour is computed refactor: some misuse of the Observer pattern

--- src/main/java/deliverif/gui/viewstate/DisplayRequestsView.java
+++ src/main/java/deliverif/gui/viewstate/DisplayRequestsView.java
@@ -5,16 +5,20 @@ import deliverif.gui.panel.GraphicalViewPanel;
 import deliverif.gui.panel.TextualViewPanel;
 import deliverif.gui.utils.Assets;
 import deliverif.gui.utils.ColorTheme;
+import deliverif.model.DeliveryTour;
+import deliverif.observer.Observable;
+import deliverif.observer.Observer;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class DisplayRequestsView extends ViewState implements ActionListener {
+public class DisplayRequestsView extends ViewState implements ActionListener, Observer {
 
     private final JButton computeButton;
-
+    private TextualViewPanel textualViewPanel;
+    private JLabel notOrdered;
     public DisplayRequestsView(Gui gui) {
         super(gui);
 
@@ -30,6 +34,8 @@ public class DisplayRequestsView extends ViewState implements ActionListener {
 
         graphicalViewPanel.revalidate(); // WARN: crucial after a removeAll()
         gui.getFrame().repaint();
+
+        gui.getController().getTour().addObserver(this);
 
 
         // add text to East panel
@@ -50,7 +56,7 @@ public class DisplayRequestsView extends ViewState implements ActionListener {
         textualViewPanel.repaint();
         */
 
-        TextualViewPanel textualViewPanel = gui.getTextualViewPanel();
+        textualViewPanel = gui.getTextualViewPanel();
         textualViewPanel.removeAll();
         JLabel pleaseLoadRequestsText = new JLabel("Compute a tour");
         textualViewPanel.add(pleaseLoadRequestsText);
@@ -59,6 +65,10 @@ public class DisplayRequestsView extends ViewState implements ActionListener {
         computeButton.addActionListener(this);
 
         //TextualViewScrollPanel textualViewScrollPanel = new TextualViewScrollPanel();
+        if (gui.getController().getTour().getPath().size()<2){
+            notOrdered = new JLabel("Request not yet ordered (Please compute a tour)");
+            textualViewPanel.add(notOrdered);
+        }
         this.gui.getRequestsTextView().init();
         textualViewPanel.add(
                 //textualViewScrollPanel
@@ -110,6 +120,15 @@ public class DisplayRequestsView extends ViewState implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == this.computeButton) {
             this.gui.getController().computingTourButtonClick(gui);
+        }
+    }
+
+    @Override
+    public void update(Observable observed, Object arg) {
+        if (((DeliveryTour)observed).getPath().size()>=2){
+            textualViewPanel.remove(notOrdered);
+            textualViewPanel.revalidate();
+            textualViewPanel.repaint();
         }
     }
 }
